6be0f0da1fc6a58d34b3e932274a4952a6a493be
Freddie1Mercury <kirillostrovskylol@mail.ru>
2023-09-24 08:03:34 +0300

    bronevik50%

--- Assets/Gameplay/Enamy/APC/MaximMachineGun.cs
+++ Assets/Gameplay/Enamy/APC/MaximMachineGun.cs
@@ -6,19 +6,45 @@ public class MaximMachineGun : MonoBehaviour
 {
 
     [SerializeField] private GameObject bullet;
+    [SerializeField] private GameObject cube;
     [SerializeField] Transform spawnBullet;
     [SerializeField] private float speedBullet;
+    private float _amountShoot = 10;
+    private float reloadMachineGun = 15f;
     private bool _isShooting = false;
-    private float _resetIshoting = 10;
 
+    private void FixedUpdate()
+    {
+        spawnBullet.LookAt(cube.transform.position);
+        reloadMachineGun -= Time.deltaTime;
+        if (reloadMachineGun <= 0)
+        {
+            for (int i = 0; i < _amountShoot; i++)
+            {
+                StartCoroutine(ShootWithDelay());
+            }
+            reloadMachineGun = 15f;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Shoot();
+        }
+    }
+    private IEnumerator ShootWithDelay()
+    {
+        for (int i = 0; i < _amountShoot; i++)
+        {
+            Shoot();
+            yield return new WaitForSeconds(0.2f); 
+        }
+    }
     public void Shoot()
     {
         if (_isShooting == true) return;
         _isShooting = true;
         GameObject bulletObject = Instantiate(bullet, spawnBullet.position, spawnBullet.rotation);
         Rigidbody rb = bulletObject.GetComponent<Rigidbody>();
-        rb.AddForce(-spawnBullet.forward * speedBullet, ForceMode.Impulse);
-        _resetIshoting = 5;
+        rb.AddForce(spawnBullet.forward * speedBullet, ForceMode.Impulse);
         StartCoroutine(DestroyBullet(bulletObject));
         StartCoroutine(ResetIsShooting());
     }
